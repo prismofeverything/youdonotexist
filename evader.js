d693c13422189a316f02d8888e44390583464d6e
Ryan Spangler <rspangler@Ryan-Spangler.local>
2008-11-22 00:57:09 -0800

    got evader working with new vector

--- evader.js
+++ evader.js
@@ -1,103 +1,107 @@
 var evader = function(spec) {
-	var that = {};
+    var that = {};
 
-	that.element = $(spec.element);
-	that.over = spec.over
-	that.out = spec.out
+    that.element = $(spec.element);
+    that.over = spec.over
+    that.out = spec.out
 
-	that.origin = vector2d({x: 500, y: 300});
-	that.threshhold = 100;
-	that.spriteliness = 2000.0;
-	that.intervalFactor = 100;
-	that.approachFactor = 1000;
+    that.origin = vector({elements: [500, 300]});
+    that.threshhold = 100;
+    that.spriteliness = 2000.0;
+    that.intervalFactor = 100;
+    that.approachFactor = 1000;
 
-	that.now = vector2d({x: 0, y: 0});
-	that.before = vector2d({x: 0, y: 0});
-	that.motion = vector2d({x: 0, y: 0});
-	that.offset = vector2d({x: -25, y: -20});
-	that.approached = false;
-	that.accepted = false;
-	that.rejected = false;
+    that.now = vector({elements: [0, 0]});
+    that.before = vector({elements: [0, 0]});
+    that.motion = vector({elements: [0, 0]});
+    that.offset = vector({elements: [-25, -20]});
+    that.approached = false;
+    that.accepted = false;
+    that.rejected = false;
 
-	that.evading = vector2d({x: 0, y: 0});
+    that.evading = vector({elements: [0, 0]});
 
-	that.applyMotion = function() {
-		that.orient();
-		that.evade();
+    that.applyVector = function(v) {
+        that.element.style.left = '' + v.elements[0] + 'px';
+        that.element.style.top = '' + v.elements[1] + 'px';
 
-		that.origin.applyTo(that.element);
-	};
+        return that;
+    };
 
-	that.orient = function() {
-		that.direction = that.origin.difference(that.now);
-		that.distance = that.origin.distance(that.now);
-		that.motion = that.now.difference(that.before);
-	};
+    that.applyMotion = function() {
+        that.orient();
+        that.evade();
 
-	that.track = function(event) {
-		that.before = that.now;
-		that.now = vector2d({x: event.pageX, y: event.pageY}).sum(that.offset);
-		that.applyMotion();
-	};
+        that.applyVector(that.origin);
+    };
 
-	that.accept = function(event) {
-		if (!that.rejected) {
-			that.over();
-			that.accepted = true;
-		}
-	};
+    that.orient = function() {
+        that.direction = that.origin.subtract(that.now);
+        that.distance = that.origin.distance(that.now);
+        that.motion = that.now.subtract(that.before);
+    };
 
-	that.reject = function(event) {
-		that.out();
-		that.rejected = true;
-		that.element.addClassName('nothingness');
-	};
+    that.track = function(event) {
+        that.before = that.now;
+        that.now = vector({elements: [event.pageX, event.pageY]}).add(that.offset);
+        that.applyMotion();
+    };
 
-	that.decide = function() {
-		that.decided = true;
-		self = that;
-		that.interval = setInterval(function(){self.approach();}, self.intervalFactor);
-	};
+    that.accept = function(event) {
+        if (!that.rejected) {
+            that.over();
+            that.accepted = true;
+        }
+    };
 
-	that.approach = function() {
-		if(that.distance > 5 && !that.accepted) {
-			that.orient();
-			factor = that.approachFactor * (1.0 / (that.distance + 1.0));
-			that.origin.add(that.direction.inverse().normalize().scale(factor));
-			that.origin.applyTo(that.element);
-		}
-	};
+    that.reject = function(event) {
+        that.out();
+        that.rejected = true;
+        that.element.addClassName('nothingness');
+    };
 
-	that.clear = function() {
-		if(that.decided) {that.decided = false;};
-		if(that.timeout != null) {
-			clearTimeout(that.timeout);
-			that.timeout = null;
-		}
-		if(that.interval != null) {
-			clearInterval(that.interval);
-			that.interval = null;
-		}
-	};
+    that.decide = function() {
+        that.decided = true;
+        var self = that;
+        that.interval = setInterval(function(){self.approach();}, self.intervalFactor);
+    };
 
-	that.evade = function() {
-		that.clear();
+    that.approach = function() {
+        if(that.distance > 5 && !that.accepted) {
+            that.orient();
+            var factor = that.approachFactor * (8.0 / (that.distance + 1.0));
+            var new_origin = that.origin.add(that.direction.invert().normalize().scale(factor));
 
-		if(!that.rejected) {
-			if (that.distance < that.threshhold) {
-				if(that.approached == false) {
-					that.approached = true;
-				}
+            that.origin.set(new_origin);
+            that.applyVector(that.origin);
+        }
+    };
 
-                that.perpendicular = that.direction.perpendicular().normalize();
-                if (that.angle < 0) {
-                    that.perpendicular.invert();
+    that.clear = function() {
+        if(that.decided) {that.decided = false;};
+        if(that.timeout != null) {
+            clearTimeout(that.timeout);
+            that.timeout = null;
+        }
+        if(that.interval != null) {
+            clearInterval(that.interval);
+            that.interval = null;
+        }
+    };
+
+    that.evade = function() {
+        that.clear();
+
+        if(!that.rejected) {
+            if (that.distance < that.threshhold) {
+                if(that.approached == false) {
+                    that.approached = true;
                 }
 
                 //        that.factor = (that.distance == 0) ? 1 : (that.spriteliness / that.distance)
-                that.factor = ((that.spriteliness * 100) / square(that.distance));
-                that.force = that.direction.copy().normalize().scale(that.factor);
-                that.origin.add(that.force);
+                that.factor = ((that.spriteliness * 100) / that.distance.square());
+                that.force = that.direction.normalize().scale(that.factor);
+                that.origin.set(that.origin.add(that.force));
             }
 
             if(that.approached) {
--- vector.js
+++ vector.js
@@ -1,182 +1,88 @@
-var square = function(n) { return n * n };
 var lawOfCosines = function(a, b, c) {
-	var num = square(b) + square(c) - square(a);
-	var den = 2 * b * c;
+    var num = square(b) + square(c) - square(a);
+    var den = 2 * b * c;
 
-	return (den == 0) ? 0 : (Math.acos(num / den) - (Math.PI / 2));
+    return (den == 0) ? 0 : (Math.acos(num / den) - (Math.PI / 2));
 };
 
 Object.prototype.is_array = function(value) {
-	return value && 
-		typeof value === 'object' &&
-		typeof value.length === 'number' &&
-		typeof value.splice === 'function' &&
-		!(value.propertyIsEnumerable('length'));
+    return value && 
+        typeof value === 'object' &&
+        typeof value.length === 'number' &&
+        typeof value.splice === 'function' &&
+        !(value.propertyIsEnumerable('length'));
 }
 
-var vector2d = function(spec) {
-	spec = spec || {x: 0, y: 0};
-
-	var that = {
-		x: spec.x,
-		y: spec.y
-	};
-
-	that.toString = function() {
-		return '' + that.x + '|' + that.y;
-	};
-
-	that.add = function(pos) {
-		that.x += pos.x;
-		that.y += pos.y;
-
-		return that;
-	};
-
-	that.copy = function() {
-		return vector2d({x: that.x, y: that.y});
-	};
-
-    that.invert = function(pos) {
-        that.x = -that.x;
-        that.y = -that.y;
-
-        return that;
-    };
-
-    that.sum = function(pos) {
-        return vector2d({x: that.x + pos.x, y: that.y + pos.y});
-    };
-
-    that.difference = function(pos) {
-        return vector2d({x: that.x - pos.x, y: that.y - pos.y});
-    };
-    
-    that.inverse = function(pos) {
-        return vector2d({x: -that.x, y: -that.y});
-    }; 
-
-    that.perpendicular = function() { 
-        return vector2d({x: -that.y, y: that.x});
-    };
-
-    that.distance = function(pos) {
-        return Math.abs( Math.sqrt( square(that.x - pos.x) + square(that.y - pos.y) ) );
-    };
-
-    that.normalize = function() {
-        distance = that.distance(vector2d());
-
-        if (distance == 0) {
-        } else {
-            that.x /= distance;
-            that.y /= distance;
-        }
-        return that;
-    };
-
-    that.scale = function(factor) {
-        that.x *= factor;
-        that.y *= factor;
-
-        return that;
-    };
-
-    that.applyTo = function(element) {
-        element.style.left = '' + that.x + 'px';
-        element.style.top = '' + that.y + 'px';
-
-        return that;
-    };
-
-    return that;
-};
-
-Number.prototype.add = function(other) {
-	return this + other;
-}
-
-Number.prototype.subtract = function(other) {
-	return this - other;
-}
-
-Number.prototype.multiply = function(other) {
-	return this * other;
-}
-
-Number.prototype.divide = function(other) {
-	return this / other;
-}
-
-Number.prototype.invert = function() {
-	return this * -1;
-}
-
-Number.prototype.copy = function() {
-	return this;
-}
-
-Number.prototype.root = function(degree) {
-	return Math.sqrt(this);
-}
-
+Number.prototype.add = function(other) { return this + other; }
+Number.prototype.subtract = function(other) { return this - other; }
+Number.prototype.multiply = function(other) { return this * other; }
+Number.prototype.divide = function(other) { return this / other; }
+Number.prototype.invert = function() { return this * -1; }
+Number.prototype.copy = function() { return this; }
+Number.prototype.root = function(degree) { return Math.sqrt(this) }
+Number.prototype.square = function() { return this * this };
 Number.prototype.zero = 0;
 
 var vector = function(spec) {
-	spec = spec || {dim: 0, elements: [], zero: 0};
+    spec = spec || {dim: 0, elements: [], zero: 0};
 
-	var that = {};
+    var that = {};
 
-	that.dim = spec.dim || spec.elements.size() || 0;
-	that.elements = spec.elements || $R(0, that.dim).map(function(el){return 0});
-	that.zero = spec.zero || 0;
+    that.dim = spec.dim || spec.elements.size() || 0;
+    that.elements = spec.elements || $R(0, that.dim).map(function(el){return 0});
+    that.zero = spec.zero || 0;
 
-	that.toString = function() {
-		return that.elements.join('|');
-	};
+    that.toString = function() {
+        return that.elements.join('|');
+    };
 
-	that.set = function(other) {
-		var index = 0;
-		other.elements.each(function(el) {
-			that[index] = el;
-			index += 1;
-		});
-	};
+    that.set = function(other) {
+        other.elements.each(function(el, index) {
+            that.elements[index] = el;
+        });
+    };
 
-	that.copy = function() {
-		return vector({elements: that.elements.map(function(el) {
-			el.copy();
-		})});
-	};
+    that.copy = function() {
+        return vector({elements: that.elements.map(function(el) {
+            return el.copy();
+        })});
+    };
 
-	that.sum = function() {
-		return that.elements.inject(that.zero, function(s, el) {return s.add(el)});
-	}
+    that.sum = function() {
+        return that.elements.inject(that.zero, function(s, el) {return s.add(el)});
+    }
 
-	that.ply = function(other, f) {
-		return vector({elements: that.elements.zip(other.elements).map(function(el) {
-			return f(el[0], el[1]);
-		})});
-	};
+    that.ply = function(other, f) {
+        return vector({elements: that.elements.zip(other.elements).map(function(el) {
+            return f(el[0], el[1]);
+        })});
+    };
 
     that.add = function(other) {
-		return that.ply(other, function(a, b) {return a.add(b)});
+        return that.ply(other, function(a, b) {return a.add(b)});
     };
 
     that.subtract = function(other) {
-		return that.ply(other, function(a, b) {return a.subtract(b)});
+        return that.ply(other, function(a, b) {return a.subtract(b)});
     };
     
     that.invert = function() {
-		return vector({elements: that.elements.map(function(el) {
-			return el.invert();
-		})});
+        return vector({elements: that.elements.map(function(el) {
+            return el.invert();
+        })});
     };
 
     that.distance = function(other) {
-		return that.ply(other, function(a, b) {
-			return square(a.subtract(b));
-		}).sum().root(2);
+		var plied = that.ply(other, function(a, b) {
+			var diff = a.subtract(b)
+			var squared = diff.square();
+			return squared;
+		});
+
+		var summed = plied.sum();
+		var rooted = summed.root(2);
+		
+		return rooted;
     };
 
     that.normalize = function() {
