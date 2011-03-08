adc8709463e38a566b8704249f53f41267edeed5
ryan spangler <ryan.spangler@gmail.com>
2011-03-08 12:46:54 -0800

    neuron

--- js/neural.js
+++ js/neural.js
@@ -1,6 +1,12 @@
 var neural = function() {
     var neurons = [];
 
+    var neuron = linkage.type({
+        init: function() {
+            
+        }
+    });
+
     var world = flux.canvas({
         id: 'neural',
         motes: neurons
